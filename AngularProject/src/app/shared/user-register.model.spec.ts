751288e225f893ae966cdebb351540ef2b3d0390
sumuongit <s.ahmed@stscapital.lt>
2020-05-03 01:01:21 +0600

    Remove spec.ts files

--- AngularProject/src/app/shared/user-register.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserRegister } from './user-register.model';
-
-describe('UserRegister', () => {
-  it('should create an instance', () => {
-    expect(new UserRegister()).toBeTruthy();
-  });
-});
